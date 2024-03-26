08e7a32f125267260a7830da44ed4d372ad83faa
czeenda <j.seycek@gmail.com>
2024-03-26 23:19:01 +0100

    variables

--- src/supabase/client.jsx
+++ src/supabase/client.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const projectURL = import.meta.env.VITE_SUPABASE_PROJECT_URL;
-const projectKey = import.meta.env.VITE_SUPABASE_PROJECT_KEY;
+const projectURL = VITE_SUPABASE_PROJECT_URL;
+const projectKey = VITE_SUPABASE_PROJECT_KEY;
 
 export const supabase = createClient(projectURL, projectKey);
