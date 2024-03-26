db89cc59ee4f82f6b3827595746181d85fa52843
czeenda <j.seycek@gmail.com>
2024-03-26 23:21:57 +0100

    manualy

--- src/supabase/client.jsx
+++ src/supabase/client.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const projectURL = VITE_SUPABASE_PROJECT_URL;
-const projectKey = VITE_SUPABASE_PROJECT_KEY;
+const projectURL = "https://wafqodtugwwrcampnrck.supabase.co";
+const projectKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhZnFvZHR1Z3d3cmNhbXBucmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MDYzMDUsImV4cCI6MjAyNTQ4MjMwNX0.zq0eg2vI8RUhGRUAvhMDewgiKwenHKOUAjNMQ3W3r-4";
 
 export const supabase = createClient(projectURL, projectKey);
